fc6845fb0fc7f559ed99b5a5f2e7dc514370521a
user <you@example.com>
2016-09-06 08:59:51 +0300

    bez grabel

--- utils/extract_ner_person_and_organization.py
+++ utils/extract_ner_person_and_organization.py
@@ -2,46 +2,27 @@ import hashlib
 import json, sys
 import os, os.path
 
-def push(hash_table, key, value):
-  n = len(hash_table)
-  for i in range(n):
-    if key == hash_table[i][0]:
-      hash_table[i].append(value)
-      return
-  hash_table.append([key, value])
-
-def to_string(hash_table):
-  res = '{\n'
-  n = len(hash_table)
-  for r in range(n):
-    res += '  "' + hash_table[r][0] + '" : [' 
-    m = len(hash_table[r])
-    for c in range(1, m):
-      psik_or_not = ','
-      if c == m-1:
-        psik_or_not = ''
-      res += '"' + hash_table[r][c] + '"' + psik_or_not
-    psik_or_not = ',' 
-    if r == n-1:
-      psik_or_not = ''
-    res += ']' + psik_or_not + '\n'
-  res += '}'
-  return res
 
 def process_sentence(data):
+  #l = [a for a in ss['sentences'][0]['tokens'] if 'normalizedNER' in a]
   l = [[], []]
   l[0] = [[a['ner'], a['normalizedNER']] for a in data['sentences'][0]['tokens'] if 'normalizedNER' in a]
   l[1] = [[a['ner'], a['lemma']] for a in data['sentences'][0]['tokens'] if a['ner']!='O' and not ('normalizedNER' in a)]
   tmp = l[0] + l[1]
-  hash_table = []
+  d = {}
   for i in tmp:
-    push(hash_table, i[0], i[1])
-  return json.loads(to_string(hash_table))
-  
+    if not i[0] in d:
+      d[i[0]] = []
+      d[i[0]].append(i[1])
+    else:
+      d[i[0]].append(i[1])
+  return d
+    
 
 def output_ner(string1, string2):
   #dir_path_to_ner = "write/here/the/path/"
-  dir_path_to_ner = "../intermediate_data/json"
+  #dir_path_to_ner = "../intermediate_data/json"
+  dir_path_to_ner = "/home/user/000/json"
   str1md5 = hashlib.md5(string1).hexdigest()
   str2md5 = hashlib.md5(string2).hexdigest()
   filename_INput = [dir_path_to_ner + "/" + str1md5+".json", dir_path_to_ner + "/" + str2md5 + ".json"]
@@ -56,6 +37,7 @@ def output_ner(string1, string2):
   if flag:
     return
   #  raise ValueError("file not found: "+string2)
+  #hash_table = [[], []]
   res = ['', '']
   for i in range(2):
     js = open(filename_INput[i])
