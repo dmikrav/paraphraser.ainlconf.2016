12da24bf1ee953acb1d8ae81d81898b0033b05b1
vasily <elgesto@gmail.com>
2016-09-06 10:36:01 +0300

    Add load json module

---
+++ utils/get_json_by_string.py
@@ -0,0 +1,17 @@
+import sys
+import json
+import hashlib
+import os, os.path
+import inspect, os
+
+localdir =  os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
+
+def get_json(string):
+    strmd5 = hashlib.md5(string).hexdigest()
+
+    if (not os.path.exists(localdir+"/../json/"+strmd5+".json")):
+        raise ValueError("file not found: "+string)
+
+    with open(localdir+"/../json/"+strmd5+".json") as data_file:
+        return json.load(data_file)
+
