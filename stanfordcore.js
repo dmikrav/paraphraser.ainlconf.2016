eeb6a214a817e0b00c5db7233879e742e1993412
vasily <elgesto@gmail.com>
2016-09-09 17:08:56 +0300

    Fix

--- stanfordcore.js
+++ stanfordcore.js
@@ -1,20 +1,24 @@
 var _ = require('underscore')._;
 var fs = require('fs');
+var md5 = require('md5');
 
 var data = JSON.parse(fs.readFileSync(__dirname+"/dataset.json"))
 var dir = "/tmp/sen/"
-var list = []
+var listt = []
 
 _.each(data, function(record, key, list){
 	_.each(record["translations"], function(value, engine, list){
 		_.each(value["pair"], function(sen, key, list){
 
-		if (sen.index(":")!=-1)
-			console.log(sen)    		
+		var filename = md5(sen)
+	
+		
 
-		var filename = new Buffer(sen).toString('base64')
+		if (sen.indexOf(":")!=-1)
+			sen = sen.substring(sen.indexOf(":")+1)
+	
     		fs.writeFileSync(dir+filename, sen, 'utf-8')
-    		list.push(dir+filename)
+    		listt.push(dir+filename)
 			
 		}, this)
 	}, this)
@@ -22,6 +26,6 @@ _.each(data, function(record, key, list){
 
 fs.writeFileSync(dir+"list", "", 'utf-8')
 
-_.each(list, function(value, key, list){
+_.each(listt, function(value, key, list){
    fs.appendFileSync(dir+"list", value+"\n", 'utf-8')
 }, this)
